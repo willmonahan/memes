b6fd831188681b6e23079353f6b79ddf679d3ef2
willmonahan <willmonahan13@gmail.com>
2017-05-02 02:37:36 -0400

    now the bois wiggle to a sine curve
    it looks gr8

--- datboi/main.js
+++ datboi/main.js
@@ -1,6 +1,7 @@
 var img;
 var scale;
 var bois = [];
+var increment = 15;
 
 function preload() {
 	img = loadImage("images/datboi.gif");
@@ -14,6 +15,7 @@ function setup() {
 
 function draw() {
 	for (var i = 0; i < bois.length; i++) {
+		bois[i][1] = bois[i][2] + (sin(bois[i][0]/45)*increment);
 		image(img, bois[i][0], bois[i][1], img.width*(windowHeight/3)/img.height, img.height*(windowHeight/3)/img.height);
 		bois[i][0] += 3;
 	}
@@ -22,7 +24,7 @@ function draw() {
 function addBoi() {
 	var x = -img.width*(windowHeight/3)/img.height;
 	var y = Math.floor(Math.random()*(2*windowHeight/3));
-	bois[bois.length] = [x, y];
+	bois[bois.length] = [x, y, y];
 }
 
 setInterval(addBoi, 1000);
