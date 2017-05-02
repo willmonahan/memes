1512524cecdf50c3ce396d948d2ed88141e60a74
willmonahan <willmonahan13@gmail.com>
2017-05-02 01:58:53 -0400

    deleted my old attempt at making a json database of image urls
    i’m really sad this didn’t work, i might try to implement it again later

--- salad/images.js
+++
@@ -1,6 +0,0 @@
-var obj = {
-	"images":[
-		{"url":"https://assets.entrepreneur.com/static/1425479423-vince-vaughn-appearing-free-cheesy-stock-images-you-can-download-getty-3.jpg"},
-		{"url":"http://www.freedigitalphotos.net/images/img/homepage/339504.jpg"}
-	]
-};
