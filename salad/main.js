0fb5da61fe51a23a8b6a0dbf900a5663aad4f4bc
willmonahan <willmonahan13@gmail.com>
2017-05-02 01:53:05 -0400

    reorganizing and commenting code
    to make it more comprehensible

--- salad/main.js
+++ salad/main.js
@@ -2,15 +2,11 @@ var img = [];
 var salads = [];
 var previous;
 
-function preload() {
-
-}
-
-preload();
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(255);
+
+	//these for loops just pre-load all of the potential images
 
 	for(i = 1; i <=6; i++) {
 		img[i] = loadImage("images/"+i+".jpg");
@@ -21,11 +17,7 @@ function setup() {
 	}
 }
 
-function draw() {
-
-}
-
-function preserveAspect(image) {
+function preserveAspect(image) { //this function returns the necessary scale for the image to fill the window without distortion
 	imgaspect = image.width/image.height;
 	winaspect = windowWidth/windowHeight;
 
@@ -38,29 +30,32 @@ function preserveAspect(image) {
 }
 
 function reimage() {
-	var randomImage = Math.floor(Math.random()*6)+1;
-	var randomSalad = Math.floor(Math.random()*6)+1;
-	var scale = preserveAspect(img[randomImage]);
-	var saladScale = (windowHeight/4.5)/salads[randomSalad].height;
-	var width = scale*img[randomImage].width;
+	// the following lines pick and draw a background
+	var randomImage = Math.floor(Math.random()*6)+1; //generate a random stock image from the set of 6
+	while(previous == randomImage) { //this loop ensures the same image isn't used twice in a row
+		randomImage = Math.floor(Math.random()*6)+1;
+	}
+	var scale = preserveAspect(img[randomImage]); //this uses the preserveAspect function to figure out the necessary scale to fill the window without distortion
+	var width = scale*img[randomImage].width; //these are the width and height of the image
 	var height = scale*img[randomImage].height;
 	var x = 0;
 	var y = 0;
-	if(width > windowWidth) {
+	if(width > windowWidth) { //this if/else clause centers the image
 		x = (width-windowWidth)/-2;
 	}
 	else {
 		y = (height-windowHeight)/-2;
 	}
-	image(img[randomImage], x, y, width, height);
+	image(img[randomImage], x, y, width, height); //draws the image
+	previous = randomImage;
 
+	//the following lines pick and draw a salad
+	//this whole section is basically the same as the section whic draws the stock image, but for the salads
+	var randomSalad = Math.floor(Math.random()*6)+1;
+	var saladScale = (windowHeight/4.5)/salads[randomSalad].height;
 	var saladx = Math.floor(Math.random()*(windowWidth-saladScale*salads[randomSalad].width));
 	var salady = Math.floor(Math.random()*(windowHeight/3-saladScale*salads[randomSalad].height)) + 2*windowHeight/3;
-
 	image(salads[randomSalad], saladx, salady, saladScale*salads[randomSalad].width, saladScale*salads[randomSalad].height);
-	count++
-	if(count > 2) {
-		count = 1;
-	}
 }
-setInterval(reimage, 1500);
+
+setInterval(reimage, 1150); //this calls the function to redraw recursively
